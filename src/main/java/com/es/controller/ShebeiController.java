b2473df9d763fd32a96eb2b6c0f8d1b70c4dd6f3
zth2020 <z15938361207@163.com>
2020-12-04 16:22:27 +0800

    ‘’

--- src/main/java/com/es/controller/ShebeiController.java
+++ src/main/java/com/es/controller/ShebeiController.java
@@ -5,8 +5,11 @@ import com.es.entity.Shebei;
 import com.es.service.ShebeiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.List;
 
 @RestController
@@ -21,5 +24,19 @@ public class ShebeiController {
 
        return shebeiService.findList(new Shebei());
 
+    }
+
+    @RequestMapping("test")
+    public void response(HttpServletResponse response) throws Exception{
+
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8;");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8;");
+        PrintWriter writer=response.getWriter();
+        writer.write(
+                "<script type=\"text/javascript\">alert('导出失败，请重试！...');</script>");
+        writer.flush();
+        writer.close();
     }
 }
